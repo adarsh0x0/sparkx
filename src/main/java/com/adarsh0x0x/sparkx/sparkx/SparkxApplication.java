0ba480560b7a8015a315cce40a8383846484a1a3
Adarsh Thapa <realadarshthapa@gmail.com>
2021-01-28 07:53:06 +0545

    initial state

---
+++ src/main/java/com/adarsh0x0x/sparkx/sparkx/SparkxApplication.java
@@ -0,0 +1,13 @@
+package com.adarsh0x0x.sparkx.sparkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adarsh0x0x/sparkx/sparkx/SparkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh0x0x.sparkx.sparkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
